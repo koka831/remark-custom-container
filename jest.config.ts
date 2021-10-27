a32d4573ee672eeb8c2cce57a3fab7d306ccf66f
koka <koka.code@gmail.com>
2021-10-27 18:49:18 +0900

    fix test

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,13 @@
 import { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  extensionsToTreatAsEsm: [".ts"],
+  preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
-  transform: {},
-  // transform: { "\\.ts$": "ts-jest" },
+  transform: { "\\.ts$": "ts-jest" },
   globals: {
     "ts-jest": {
       useESM: true,
-      // tsConfig: "tsconfig.esm.json",
+      tsConfig: "tsconfig.esm.json",
     },
   },
   moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
