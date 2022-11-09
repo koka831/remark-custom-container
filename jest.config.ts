e9974bfa9a5f3595a029bbc3e24d608be25a6516
koka <koka.code@gmail.com>
2022-11-09 21:29:05 +0900

    update dev dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,14 @@ import { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
-  transform: { "\\.ts$": "ts-jest" },
-  globals: {
-    "ts-jest": {
-      useESM: true,
-      tsConfig: "tsconfig.esm.json",
-    },
+  transform: {
+    "\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "tsconfig.esm.json",
+      },
+    ],
   },
   moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
 };
